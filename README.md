cc1e1face1e03f5b354218a445aa6d05cb96787d
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-01-24 16:14:19 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Skills 3
+
+<sup>:apple: Hackbright Academy Software Engineering Program</sup>
+
+### [:link: Click here to view instructions for this skills assessment](https://fellowship.hackbrightacademy.com/materials/homework/skills-3/)
---
+++ server.py
@@ -0,0 +1,54 @@
+from flask import Flask, redirect, request, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+
+# This option will cause Jinja to throw UndefinedErrors if a value hasn't
+# been defined (so it more closely mimics Python's behavior)
+app.jinja_env.undefined = StrictUndefined
+
+# This option will cause Jinja to automatically reload templates if they've been
+# changed. This is a resource-intensive operation though, so it should only be
+# set while debugging.
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = 'ABC'
+
+MOST_LOVED_MELONS = {
+    'cren': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimRegular/crenshaw.jpg',
+        'name': 'Crenshaw',
+        'num_loves': 584,
+    },
+    'jubi': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Jubilee-Watermelon-web.jpg',
+        'name': 'Jubilee Watermelon',
+        'num_loves': 601,
+    },
+    'sugb': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Sugar-Baby-Watermelon-web.jpg',
+        'name': 'Sugar Baby Watermelon',
+        'num_loves': 587,
+    },
+    'texb': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Texas-Golden-2-Watermelon-web.jpg',
+        'name': 'Texas Golden Watermelon',
+        'num_loves': 598,
+    },
+}
+
+
+# REPLACE THIS WITH YOUR ROUTES
+
+
+if __name__ == '__main__':
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    DebugToolbarExtension(app)
+
+    app.run(host='0.0.0.0')
